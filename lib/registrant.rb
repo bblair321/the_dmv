9a8455b303748a65fe9383ab33cccca381829517
Brady Blair <bblair321@yahoo.comm>
2025-02-13 07:57:34 -0800

    added instance variables to registrant class

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,5 +1,9 @@
 class Registrant
-  def initialize
-    
+  attr_reader
+  def initialize(name,age,permit,license_data)
+    @name = name
+    @age = age
+    @permit = permit
+    @license_data = license_data
   end
 end
