3bccfb4974f28436b97d5fe52b5465908fd6fb04
Brady Blair <bblair321@yahoo.comm>
2025-02-13 12:42:28 -0800

    Added @registered_vehicles to facility class. As well as a test

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,5 +1,5 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :registered_vehicles
 
   def initialize(attributes)
     # switched to a hash instead of individual arguments
@@ -7,9 +7,11 @@ class Facility
     @address = attributes[:address]
     @phone = attributes[:phone]
     @services = []
+    @registered_vehicles = []
   end
 
   def add_service(service)
     @services << service
   end
+
 end
--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
+require './lib/facility'
+require './lib/vehicle'
 
 RSpec.describe Facility do
   before(:each) do
     @facility = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
   end
-  describe '#initialize' do
+  describe 'initialize' do
     it 'can initialize' do
       expect(@facility).to be_an_instance_of(Facility)
       expect(@facility.name).to eq('DMV Tremont Branch')
@@ -14,13 +16,23 @@ RSpec.describe Facility do
     end
   end
 
-  describe '#add service' do
+  describe 'add service' do
     it 'can add available services' do
       expect(@facility.services).to eq([])
       @facility.add_service('New Drivers License')
       @facility.add_service('Renew Drivers License')
       @facility.add_service('Vehicle Registration')
       expect(@facility.services).to eq(['New Drivers License', 'Renew Drivers License', 'Vehicle Registration'])
+    end
+  end
+  describe '' do
+    it 'can check registered vehicles' do
+      facility_1 = Facility.new({name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205', phone: '(720) 865-4600'})
+      cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
+      facility_1.add_service('Vehicle Registration')
+
+
+      expect(facility_1.registered_vehicles).to eq []
     end
   end
 end
